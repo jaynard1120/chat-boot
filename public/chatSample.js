34a5fe6a00159720a0121f70785956f10c7548c2
1styrGroupB <1styrGroupB@pn.local>
2020-12-07 12:55:47 +0800

    Alert fixed

--- public/chatSample.js
+++ public/chatSample.js
@@ -8,8 +8,7 @@ $(function () {
 
   $('#out').click(()=>{
     Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
+      title: 'Are you sure you want to leave?',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
