174701ac39a44b7f94ce6de02789128d2394dd49
1styrGroupB <1styrGroupB@pn.local>
2020-12-07 12:51:03 +0800

    Button fixed

--- public/chatSample.js
+++ public/chatSample.js
@@ -5,6 +5,23 @@ $(function () {
     var element = document.getElementById("msg");
     element.scrollTop = element.scrollHeight;
 }
+
+  $('#out').click(()=>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = '/'
+      }
+    })
+  })
+
   appendMessage("<center><div>You joined the group chat</div></center>  ")
   socket.emit('new-user', name);
   socket.emit('onlines');
